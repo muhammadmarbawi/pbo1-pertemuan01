7895b82065ed24bbfe612b975a75e1fce11b44e5
marbawi <muhammadmarbawi05@gmail.com>
2024-03-20 22:47:31 +0800

    Hello Git & Java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
-public class hello{
- public static void main (string [] args){
-  system.out.println("Hello World  !!");
+public class Hello{
+ public static void main (String [] args){
+  System.out.println("Hello Git  !!");
+  System.out.println("Hello Java  !!");
  }
 }
